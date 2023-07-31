ee7a2d349ac93d0df1d906a9ce1a2fd5c8ad890c
R8nDr0p <rainchrstn@gmail.com>
2023-07-31 20:54:45 +0800

    made a log in and sign up page in order to get the two players

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import Axios from "axios";
+import Cookies from "universal-cookie";
 
-function Login() {
+function Login({ setIsAuth }) {
+  const [username, setUserName] = useState("");
+  const [password, setPassWord] = useState("");
+
+  const cookies = new Cookies();
+  const logIn = () => {
+    // code goes here
+    Axios.post("http://localhost:3001/login", {
+      username,
+      password,
+    }).then((res) => {
+      const { firstName, lastName, username, token, userId } = res.data;
+      cookies.set("token", token);
+      cookies.set("userId", userId);
+      cookies.set("username", username);
+      cookies.set("firstName", firstName);
+      cookies.set("lastName", lastName);
+      setIsAuth(true);
+    });
+  };
   return (
     <>
-      <div>Login</div>
+      <div className="login">
+        <label>Log In</label>
+        <input
+          value={username}
+          type="text"
+          placeholder="Username"
+          onChange={(event) => {
+            setUserName(event.target.value);
+          }}
+        />
+        <input
+          value={password}
+          type="password"
+          placeholder="Password"
+          onChange={(event) => {
+            setPassWord(event.target.value);
+          }}
+        />
+        <button onClick={logIn}>Log in</button>
+      </div>
     </>
   );
 }
--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,10 +1,31 @@
 import React, { useState } from "react";
+import axios from "axios";
+import Cookies from "universal-cookie";
 
-function SignUp() {
+function SignUp({ setIsAuth }) {
+  const cookies = new Cookies();
   const [user, setUser] = useState(null);
+  const signUp = () => {
+    // code goes here
+    try {
+      axios.post("http://localhost:3001/signup", user).then((res) => {
+        const { token, userId, firstName, lastName, username, hashedPassword } =
+          res.data;
+
+        cookies.set("token", token);
+        cookies.set("userId", userId);
+        cookies.set("firstName", firstName);
+        cookies.set("lastName", lastName);
+        cookies.set("username", username);
+        cookies.set("hashedPassword", hashedPassword);
+        setIsAuth(true);
+      });
+    } catch (error) {}
+  };
+
   return (
     <>
-      <div>
+      <div className="signup">
         <label htmlFor="">Sign Up</label>
         <input
           type="text"
@@ -24,16 +45,17 @@ function SignUp() {
           type="text"
           placeholder="Username"
           onChange={(event) => {
-            setUser({ ...user, userName: event.target.value });
+            setUser({ ...user, username: event.target.value });
           }}
         />
         <input
-          type="text"
-          placeholder="Last Name"
+          type="password"
+          placeholder="Password"
           onChange={(event) => {
-            setUser({ ...user, passWord: event.target.value });
+            setUser({ ...user, password: event.target.value });
           }}
         />
+        <button onClick={signUp}>Sign up</button>
       </div>
     </>
   );
